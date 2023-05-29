7d8ee6503ead88ccef9b95fad85e26792e38c338
CarlosContrerasQ12 <cadacoqu2@live.com>
2023-05-28 23:25:59 -0500

    Chp3

--- Tesis/code/pdeSolver/domains.py
+++ Tesis/code/pdeSolver/domains.py
@@ -28,6 +28,149 @@ spec = [
 ]
 """
 
+class Domain():
+    """
+    An abstract class containing all functions that should provide a domain.
+    """
+    def simulate_diffusion_path(self,sigma,dt,t0,X0,Nmax,dirichlet_cut=False,neumann_cut=False):
+        raise("simulate diffusion path not implemented yet")
+    def simulate_controlled_diffusion_path(self,drift,sigma,dt,t0,X0,Nmax,dirichlet_cut=False,neumann_cut=False):
+        raise("simulate_controlled_diffusion_not_implemented yet")
+    
+class GoodEmptyRoom(Domain):
+    """A class representing a domain [0,1]x[0,1] that is an empty room with one exit door
+    -N is the number of people inside the room
+    -total_time is the end time of the simulation
+    -pInf is the lower part of the door in the y axis
+    -pSup is the Upper part of the door in the y axis
+    """
+    def __init__(self,dom_config):
+        self.dom_config=dom_config
+        self.dim=2
+        self.pInf=dom_config["pInf"] #Lower point of the door in the y axis
+        self.pSup=dom_config["pSup"] #Upper point of the door in the y axis
+        self.pAnc=self.pSup-self.pInf #Width of thr door 
+
+    def exited_domain(self,X0,X1):
+        """
+        If the step from X0 to X1 exits the domain, it returns the following:
+        -The point at which it touches the boundary
+        -The type of boundary that it touched 'Neu' for the Neumann part 'Dir' for dirichlet part
+        -The fraction of dt at which the movement touched it
+        """
+        if X1[0]<0 or X1[0]>1 or X1[1]>1 or X1[1]<0:
+            v=X1-X0
+            tleft=-X0[0]/(v[0])
+            tup=(1-X0[1])/(v[1])
+            trig=(1-X0[0])/(v[0])
+            tdow=-X0[1]/(v[1])
+            ts=np.array2([tleft,tup,trig,tdow])
+            ts=np.where(ts>1,10.0,ts)
+            ts=np.where(ts<0,10.0,ts)
+            i=np.argmin(ts)
+
+            if i==0:
+                Xi=np.array2([0.0,X0[1]+ts[i]*(v[1])])
+                return Xi,'Neu',ts[i]
+            elif i==1:
+                Xi=np.array2([X0[0]+ts[i]*(v[0]),1.0])
+                return Xi,'Neu',ts[i]
+            elif i==2:
+                Xi=np.array2([1.0,X0[1]+ts[i]*(v[1])])
+                if Xi[1]>self.pInf and Xi[1]<self.pSup:
+                    return Xi,'Dir',ts[i]
+                else:
+                    return Xi,'Neu',ts[i]
+            elif i==3:
+                Xi=np.array2([X0[0]+ts[i]*(v[0]),0.0])
+                return Xi,'Neu',ts[i]
+        return X1,'Non',1.0
+    
+    def simulate_diffusion_path(self,sigma,dt,t0,X0,Nmax,dirichlet_cut=False,neumann_cut=False):
+        """ 
+        Simulates ones2 brownian motion in the domain with the following parameters:
+        sig:: volatility of motion
+        dt:: time step
+        Nmax: maximum length of path
+        t0: Starting time of simulation
+        X0: Starting point
+        dirichlet_cut: If true, the motion stops if it touches the dirihclet boundary.
+        If false, it stays there until Nmax is reached
+        neumann_cut: If true, the motion stops if it reaches the neumann boundary.
+        If false, it simulates a reflected motion.
+
+        """
+        sqdt=np.sqrt(dt)
+        X=np.zeros2((Nmax,2))
+        X[0]=X0
+        xis=np.random.normal2(loc=0,scale=1,size=(Nmax-1,2))
+        dtf=1.0
+        for i in range(1,Nmax):
+            Xnew=X[i-1]+sqdt*sigma*xis[i-1]
+            Xnew,exit,dtf=self.exited_domain(X[i-1],Xnew)
+            if exit=='Neu':
+                if not neumann_cut:
+                    Xn=Xnew+np.sqrt(dt*(1-dtf))*sigma*np.random.normal2(loc=0,scale=1,size=2)
+                    Xs,e,dtf2=self.exited_domain(Xnew,Xn)
+                    while e!='Non':
+                        Xn=Xnew+np.sqrt(dt*(1-dtf))*sigma*np.random.normal2(loc=0,scale=1,size=2)
+                        Xs,e,dtf2=self.exited_domain(Xnew,Xn)
+                    X[i]=Xs
+                    xis[i-1]=(X[i]-X[i-1])/(sqdt*sigma)
+                else:
+                    X[i]=Xnew
+                    return X[:i+1],xis[:i],dtf
+            elif exit=='Dir':
+                if not dirichlet_cut:
+                    X[i]=Xnew
+                    X[i+1:]=np.array2([1.0,self.pInf+0.5*self.pAnc])
+                    xis[i:]=np.zeros2(2)
+                    return X,xis,dtf
+                else:
+                    X[i]=Xnew
+                    return X[:i+1],xis[:i],dtf
+            elif exit=='Non':
+                X[i]=Xnew
+        return X,xis,dtf
+    
+    def simulate_controlled_diffusion_path(self,drift,sigma,dt,t0,X0,Nmax,dirichlet_cut=False,neumann_cut=False):
+        sqdt=np.sqrt(dt)
+        X=np.zeros2((Nmax,2))
+        X[0]=X0
+        xis=np.random.normal2(loc=0,scale=1,size=(Nmax-1,2))
+        dtf=1.0
+        t=t0
+        for i in range(1,Nmax):
+            mu=drift(t,X[i-1])
+            Xnew=X[i-1]+mu*dt+sqdt*sigma*xis[i-1]
+            Xnew,exit,dtf=self.exited_domain(X[i-1],Xnew)
+            if exit=='Neu':
+                if not neumann_cut:
+                    mu=drift(t,Xnew)
+                    Xn=Xnew+mu*dt+np.sqrt(dt*(1-dtf))*sigma*np.random.normal2(loc=0,scale=1,size=2)
+                    Xs,e,dtf2=self.exited_domain(Xnew,Xn)
+                    while e!='Non':
+                        Xn=Xnew+mu*dt+np.sqrt(dt*(1-dtf))*sigma*np.random.normal2(loc=0,scale=1,size=2)
+                        Xs,e,dtf2=self.exited_domain(Xnew,Xn)
+                    X[i]=Xs
+                    xis[i-1]=(X[i]-X[i-1])/(sqdt*sigma)
+                else:
+                    X[i]=Xnew
+                    return X[:i+1],xis[:i],dtf
+            elif exit=='Dir':
+                if not dirichlet_cut:
+                    X[i]=Xnew
+                    X[i+1:]=np.array2([1.0,self.pInf+0.5*self.pAnc])
+                    xis[i:]=np.zeros2(2)
+                    return X,xis,dtf
+                else:
+                    X[i]=Xnew
+                    return X[:i+1],xis[:i],dtf
+            elif exit=='Non':
+                X[i]=Xnew
+            t+=dt
+        return X,xis,dtf
+
 #@jitclass(spec)
 class EmptyRoom():
     """A class representing a domain [0,1]x[0,1] that is an empty room with one exit door
